51aecd12b91e51092f71a005126ebc541cad332a
felipesfl <felipeferreira.desenvolvedor@gmail.com>
2026-07-22 10:33:18 -0300

    slide  e banner- html e talwind css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,94 @@
-const track = document.getElementById('track');
-const dotsContainer = document.getElementById('dots');
+const slides = document.getElementById("slides");
+const buttons = document.querySelectorAll(".btn");
+const slideItems = document.querySelectorAll("#slides > div");
 
-const realSlides = Array.from(track.children);
-const total = realSlides.length;
+let current = 0;
+const totalSlides = slideItems.length;
 
-const firstClone = realSlides[0].cloneNode(true);
-const lastClone = realSlides[total - 1].cloneNode(true);
-track.appendChild(firstClone);
-track.insertBefore(lastClone, track.firstChild);
+// Calcula a largura do slide + o gap
+function getSlideWidth() {
+    const slide = slideItems[0];
 
-let current = 1;
-let animating = false;
+    // gap-10 = 2.5rem = 40px
+    const gap = 40;
 
-for(let i = 0; i , total; i++){
-    const dot = document.createElement('button');
-    dot.className = 'h-2.5 rounded-full transition-all duration-300 bg-neutral-300';
-    dot.style.width = '10px';
-    dotsContainer.appendChild(dot);
+    return slide.offsetWidth + gap;
 }
 
+function updateCarousel() {
 
-function updtateDots(){
-    const realIndex = ((current - 1) + total) % total;
-    [...dotsContainer.children].forEach((dot, i) =>{
-        if(i == realIndex){
-            
+    const distance = current * getSlideWidth();
+
+    slides.style.transform = `translateX(-${distance}px)`;
+
+    buttons.forEach((button, index) => {
+
+        if (index === current) {
+            button.classList.remove("bg-gray-300");
+            button.classList.add("bg-gray-400");
+        } else {
+            button.classList.remove("bg-gray-400");
+            button.classList.add("bg-gray-300");
         }
-    }) 
+
+    });
+
 }
+
+// Clique nas bolinhas
+buttons.forEach((button, index) => {
+
+    button.addEventListener("click", () => {
+
+        current = index;
+
+        updateCarousel();
+
+        restartAutoSlide();
+
+    });
+
+});
+
+// Próximo slide
+function nextSlide() {
+
+    current++;
+
+    if (current >= totalSlides) {
+        current = 0;
+    }
+
+    updateCarousel();
+
+}
+
+// Slide anterior
+function previousSlide() {
+
+    current--;
+
+    if (current < 0) {
+        current = totalSlides - 1;
+    }
+
+    updateCarousel();
+
+}
+
+// Auto Slide
+let autoSlide = setInterval(nextSlide, 5000);
+
+function restartAutoSlide() {
+
+    clearInterval(autoSlide);
+
+    autoSlide = setInterval(nextSlide, 5000);
+
+}
+
+// Atualiza caso a janela seja redimensionada
+window.addEventListener("resize", updateCarousel);
+
+// Inicializa
+updateCarousel();
